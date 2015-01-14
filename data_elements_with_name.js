1a7a95df277e3defe71c60362c0ec12bcdfe4e06
MD Dubey <mdinfotech007@gmail.com>
2015-01-14 19:24:46 +0530

    Added logic for adding datavalues with name also.

--- data_elements_with_name.js
+++ data_elements_with_name.js
@@ -7,27 +7,27 @@ var dataSetUuid = 'iUz0yoVeeiZ';
 
 var options = {
 	hostname: host,
-	port:port,
-    auth: 'admin:district'
+	port: port,
+	auth: 'admin:district'
 };
 
-var getPath = function(uriSuffix){
-	var path = '/dhis/api/dataSets/UUID/URISUFFIX';
-	return path.replace('UUID',dataSetUuid).replace('URISUFFIX',uriSuffix);
+var getPath = function(uriSuffix) {
+	var path = '/dhis/api/dataSets/UUIDURISUFFIX';
+	return path.replace('UUID', dataSetUuid).replace('URISUFFIX', uriSuffix);
 }
 
-var writeToFile = function(dataToWrite){
-	var fileName = './data_values/UUID.json';
-	fileName = fileName.replace('UUID',dataSetUuid);
-	fs.writeFile(fileName,dataToWrite);
+var writeToFile = function(dataToWrite) {
+	var fileName = './jsons/data_values/UUID.json';
+	fileName = fileName.replace('UUID', dataSetUuid);
+	fs.writeFile(fileName, dataToWrite);
 }
 
-var addNamesToDataValues = function(jsonData, dataElementMap){
-	fs.readFile('./categoryOptionCombos.json',function(err,data){
-		if(err)
+var addNamesToDataValues = function(jsonData, dataElementMap) {
+	fs.readFile('./jsons/categoryOptionCombos.json', function(err, data) { //assuming categoryOptions are available using script
+		if (err)
 			throw err;
 		var categoryOptionComboMap = JSON.parse(data);
-		jsonData.dataValues.forEach(function(dataValue){
+		jsonData.dataValues.forEach(function(dataValue) {
 			delete dataValue.period;
 			dataValue.dataElementName = dataElementMap[dataValue.dataElement];
 			dataValue.categoryOptionName = categoryOptionComboMap[dataValue.categoryOptionCombo];
@@ -36,26 +36,43 @@ var addNamesToDataValues = function(jsonData, dataElementMap){
 	});
 }
 
-var formatDataValues = function(dataElementMap){
-	return function(data){
+var formatDataValues = function(dataElementMap) {
+	return function(data) {
 		var jsonData = JSON.parse(data);
-		addNamesToDataValues(jsonData,dataElementMap);
+		addNamesToDataValues(jsonData, dataElementMap);
 	};
 };
 
-var onDataElements = function(data){
+var onDataElements = function(data) {
 	var jsonData = JSON.parse(data);
-	var dataElementMap = jsonData.dataElements.reduce(function(obj, val){
-	    obj[val.id] = val.name;
-	    return obj;
-	}, {});	
+	var dataElementMap = jsonData.dataElements.reduce(function(obj, val) {
+		obj[val.id] = val.name;
+		return obj;
+	}, {});
 	options.path = getPath('/dataValueSet.json');
-	helper.makeGetRequest(options,formatDataValues(dataElementMap));
+	helper.makeGetRequest(options, formatDataValues(dataElementMap));
 };
 
-var main = function(){
+var getDataElements = function() {
 	options.path = getPath('/dataElements.json');
-	helper.makeGetRequest(options,onDataElements);
+	helper.makeGetRequest(options, onDataElements);
+};
+
+var onDataSet = function(data) {
+	var jsonData = JSON.parse(data);
+	var pathToAllDatasets = './jsons/datasets.json';
+	fs.readFile(pathToAllDatasets, function(err, data) { //assuming the file exists and there is data as {} atleast;
+		if (err) throw err;
+		var datasets = JSON.parse(data);
+		datasets[jsonData.id] = jsonData.name;
+		fs.writeFile(pathToAllDatasets, JSON.stringify(datasets));
+	});
+	getDataElements();
+};
+
+var main = function() {
+	options.path = getPath('.json');
+	helper.makeGetRequest(options, onDataSet);
 };
 
 main();
